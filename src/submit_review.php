2e3165880a5a709d46243b6b97e38091cd7b38ba
Dimas Kiperman <126020343+kiper7777@users.noreply.github.com>
2025-11-21 00:23:43 +0000

    PHP_MySQL
    - transferred the project to PHP;
    - created the luna_reviews_db database in MySQL;
    - created a table - reviews.

---
+++ src/submit_review.php
@@ -0,0 +1,17 @@
+<?php
+require 'luna_reviews_db.php';
+
+
+$name = $_POST['name'];
+$rating = $_POST['rating'];
+$message = $_POST['message'];
+
+
+$stmt = $conn->prepare("INSERT INTO reviews (name, rating, message) VALUES (?, ?, ?)");
+$stmt->bind_param("sis", $name, $rating, $message);
+$stmt->execute();
+$stmt->close();
+
+
+echo "success";
+?>
